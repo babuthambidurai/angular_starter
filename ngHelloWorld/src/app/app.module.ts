1d189c8149fe658a51ec5274a468a002211657b1
Babu Sriram Thambidurai <babuthambidurai@BGs-MacBook-Pro.local>
2018-03-21 00:27:14 +0530

    App Pipes, Reactive Forms

--- ngHelloWorld/src/app/app.module.ts
+++ ngHelloWorld/src/app/app.module.ts
@@ -2,25 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './components/app.component';
 import { AppMenuComponent } from './app-menu/app-menu.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerService } from './customer.service';
 import { FormsComponent } from './forms/forms.component';
-
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppMenuComponent,
     CustomersComponent,
-    FormsComponent
+    FormsComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     CustomerService
