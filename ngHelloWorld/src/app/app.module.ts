c84be81c92515c39d2cd4ed111f495d006a1f23a
Babu Sriram Thambidurai <babuthambidurai@BGs-MacBook-Pro.local>
2018-03-21 02:09:03 +0530

    PrimeNG components

--- ngHelloWorld/src/app/app.module.ts
+++ ngHelloWorld/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import {HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import {DropdownModule, ButtonModule, ChartModule} from 'primeng/primeng';
 
 import { AppComponent } from './components/app.component';
 import { AppMenuComponent } from './app-menu/app-menu.component';
@@ -11,6 +13,7 @@ import { CustomerService } from './customer.service';
 import { FormsComponent } from './forms/forms.component';
 import { SignupComponent } from './signup/signup.component';
 import { TestComponent } from './test/test.component';
+import { PrimengComponent } from './primeng/primeng.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +22,18 @@ import { TestComponent } from './test/test.component';
     CustomersComponent,
     FormsComponent,
     SignupComponent,
-    TestComponent
+    TestComponent,
+    PrimengComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DropdownModule,
+    ButtonModule,
+    ChartModule
   ],
   providers: [
     CustomerService
